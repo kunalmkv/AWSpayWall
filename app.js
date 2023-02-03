55e906d3feefe992f37a972093bba2e44daf17fb
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-02-03 17:30:32 +0530

    1st deploy

---
+++ app.js
@@ -0,0 +1,65 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const user = require('./models/user');
+const userWallet = require('./models/wallet');
+const Order = require('./models/orders');
+const sequelize = require('./util/database');
+const purchaseRoutes = require('./routes/purchase');
+const premiumRoute = require('./routes/premium');
+
+
+// get config vars
+dotenv.config();
+
+
+
+var cors = require('cors');
+
+const newUserRoutes = require('./routes/newUser');
+const existingUserRoutes = require('./routes/existingUser');
+const expenseRoutes = require('./routes/expense');
+const passwordRoutes = require('./routes/password');
+
+
+const PORT = process.env.port || 3000;
+
+
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+app.use('/premium', premiumRoute);
+app.use('/newUser', newUserRoutes);
+app.use('/existingUser', existingUserRoutes);
+app.use('/expense', expenseRoutes);
+app.use('/purchase', purchaseRoutes);
+app.use('/password', passwordRoutes);
+app.use('/user', expenseRoutes);
+
+
+
+user.hasMany(userWallet);
+userWallet.belongsTo(user);
+
+user.hasMany(Order);
+Order.belongsTo(user);
+sequelize.sync().then(result => {
+
+    //  https
+    // .createServer({ key: privateKey, cert: certificate }, app)
+    //  .listen(PORT);
+    app.listen(PORT);
+})
+    .catch(err => {
+        console.log(err);
+    })
---
+++ public/login.js
@@ -0,0 +1,56 @@
+
+async function newUserSave(event) {
+    event.preventDefault();
+    const newID = event.target.newUserID.value;
+    const mail = event.target.newEmail.value;
+    const pw = event.target.newPW.value;
+
+    const obj = {
+        newID,
+        mail,
+        pw
+    }
+    try {
+        await axios.post("http://localhost:3000/newUser/add", obj).then(response => {
+
+            alert('Registered Successfully. Login!!');
+        })
+    }
+    catch (err) {
+
+        alert('Oopss! User exists Already!! Login Please');
+    }
+
+}
+async function userLogin(event) {
+    event.preventDefault();
+    const mail = event.target.email.value;
+    const pw = event.target.pw.value;
+    const loginDetail = {
+        mail,
+        pw
+    }
+    try {
+        await axios.post("http://localhost:3000/existingUser/login", loginDetail).then(response => {
+            if (response.status === 200) {
+                alert(response.data.message);
+                localStorage.setItem('token', response.data.token);
+                window.location.href = "walletGUI.html"
+            }
+            else {
+                alert(response.data.message);
+            }
+        })
+    }
+    catch (err) {
+
+        console.log('****Not login***', JSON.stringify(err));
+        document.body.innerHTML += `<div style="color:red;"> ${err.message} <div>`
+    }
+
+}
+
+function showforgetpasswordForm() {
+    window.location.href = "forgotForm.html"
+
+}
